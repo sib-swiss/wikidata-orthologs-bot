b7598b4fa79d5b19d6fcd45c7a8c67a86631cedc
Vincent Emonet <vincent.emonet@gmail.com>
2024-05-14 17:58:28 +0200

    improve bot, now working overall

--- README.md
+++ README.md
@@ -1,33 +1,39 @@
 # 🧬 Wikidata Orthologs Bot 🤖
 
-> This repository uses [`hatch`](https://hatch.pypa.io/latest/) to easily handle scripts and virtual environments. Checkout the `pyproject.toml` file for more details on the scripts available.
+> This repository uses [`hatch`](https://hatch.pypa.io/latest/) to easily handle scripts and virtual environments. Checkout the `pyproject.toml` file for more details on the scripts available. 
+>
+> You can also just install dependencies with `pip install .` and run the python script in `src`
 
 ## Add orthologs from OMA
 
-Extension of [SuLab OrthologBot.py](https://github.com/SuLab/scheduled-bots/blob/main/scheduled_bots/geneprotein/OrthologBot.py) to include [OMA orthologs](https://omabrowser.org/oma/home/).
+Extension of [SuLab OrthologBot.py](https://github.com/SuLab/scheduled-bots/blob/main/scheduled_bots/geneprotein/OrthologBot.py) to include [OMA orthologs](https://omabrowser.org/oma/home/) and references to the OMA browser (e.g. https://omabrowser.org/oma/vps/P04637/).
 
-1. Download orthologs data:
+Define the Wikidata bot username and password in a `.env` file at the root of the repository:
 
-    ```bash
-    mkdir -p data
-    cd data
-    wget https://www.bgee.org/ftp/current/homologous_genes/OMA_orthologs.zip
-    unzip OMA_orthologs.zip
-    cd ..
-    ```
+```bash
+WDUSER=BOT_USERNAME
+WDPASS=BOT_PASSWORD
+```
 
-2. Run mapping script in parallel, will generate a CSV file with all orthologs:
+Run mapping script without writing to Wikidata, will generate a CSV file with all orthologs:
 
-    ```bash
-    hatch run oma
-    ```
+```bash
+hatch run oma
+```
 
-    > You can manually choose the number of workers with `hatch run oma 32`
-    >
-    > It takes about 90 minutes to complete on 32 cores
+Run the mapping script with writing to Wikidata enabled:
+
+```bash
+hatch run oma --write
+```
+
+> [!WARNING]
+>
+> It currently does not check if the OMA browser reference already exists in Wikidata, so it might create duplicates references if ran multiple times with `--write` enabled
 
 ## See also
 
 We use [WikidataIntegrator](https://github.com/SuLab/WikidataIntegrator) to interact with WikiData.
 
 https://github.com/BgeeDB/Wikidata_BgeeDB-bot
+
--- src/wikidata_orthologs_bot/oma.py
+++ src/wikidata_orthologs_bot/oma.py
@@ -1,25 +1,73 @@
 import argparse
 import os
+import re
 import time
+import zipfile
 from collections import defaultdict
 from dataclasses import dataclass
+from datetime import datetime
 
 import pandas as pd
 import requests
-from pandarallel import pandarallel
+from dotenv import load_dotenv
 from tqdm import tqdm
-from wikidataintegrator import wdi_core, wdi_helpers
+from wikidataintegrator import wdi_core, wdi_helpers, wdi_login
+
+# Load environment variables from .env file
+load_dotenv(".env")
+
+# Inspired by https://github.com/SuLab/scheduled-bots/blob/main/scheduled_bots/geneprotein/OrthologBot.py
+
+# Test with:
+# TRP53: https://www.wikidata.org/wiki/Q14818098
+# Interesting line for TRP53 in orthologs_9606-10090.csv:
+# ENSG00000141510,ENSMUSG00000059552,Euarchontoglires,314146
+
+# Insuline: https://www.wikidata.org/wiki/Q21163221
+# ENSG00000254647,ENSMUSG00000000215,Euarchontoglires,314146
+
+# Wikidata integrator notebook example: https://public-paws.wmcloud.org/46883698/example%20ema%20bot.ipynb
+
+# This script currently adds a reference to the OMA browser URL for each ortholog pair.
+# It does not check if the ortholog pair already exists in Wikidata, so it might create duplicates if ran multiple times
 
 
 @dataclass
 class WdProp:
     instance_of = "P31"
     ensembl_gene_id = "P594"
-    ncbi_gene_id = "P351"
+    entrez_gene_id = "P351"  # aka. NCBI gene ID
+    ncbi_taxonomy_id = "P685"
     uniprot_id = "P352"
     encodes = "P688"
     ortholog = "P684"
     found_in_taxon = "P703"
+    reference_url = "P854"
+    stated_in = "P248"
+
+
+prop = WdProp()
+
+OMA_WD_ITEM_ID = "Q7104801"
+GLOBAL_REF_MODE = "STRICT_KEEP_APPEND"
+EDIT_SUMMARY = "Add orthologs from OMA (Orthologous MAtrix) database"
+DATA_DIR = "data"
+OMA_DIR = os.path.join(DATA_DIR, "oma")
+
+
+def download_oma_files():
+    zip_file_path = os.path.join(DATA_DIR, "OMA_orthologs.zip")
+    os.makedirs(OMA_DIR, exist_ok=True)
+    if not os.listdir(OMA_DIR):
+        print("Downloading OMA files")
+        if not os.path.isfile(zip_file_path):
+            resp = requests.get("https://www.bgee.org/ftp/current/homologous_genes/OMA_orthologs.zip", timeout=600)
+            with open(zip_file_path, "wb") as file:
+                file.write(resp.content)
+        with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
+            zip_ref.extractall(OMA_DIR)
+    else:
+        print("Files already exist in ./data/oma/, skipping download")
 
 
 def is_oma_url_valid(oma_url: str) -> bool:
@@ -31,149 +79,258 @@ def is_oma_url_valid(oma_url: str) -> bool:
         return False
 
 
-def aggregate_results(results, valid_orthos: list[dict[str, str]], combined_errors: dict[str, set]) -> tuple:
-    """Agregate the results and errors of the parallel processing of the ortholog pairs."""
-    for result in results:
-        if result["data"]:
-            valid_orthos.append(result["data"])
-        for error_type, error_set in result["errors"].items():
-            combined_errors[error_type].update(error_set)
-    return valid_orthos, combined_errors
-
-
-def process_row(row, id_mapper: dict[str, str]):
-    """Process a row of the ortholog pairs DataFrame."""
-    errors = defaultdict(set)
-    gene1 = str(row["gene1"])
-    gene2 = str(row["gene2"])
-
-    try:
-        prot1 = wdi_core.WDItemEngine(wd_item_id=id_mapper[gene1]).get_wd_json_representation()["claims"][
-            WdProp.encodes
-        ]
-    except Exception as _e:
-        errors["missing_encodes_infos"].add(gene1)
-        errors["pairs_lost_due_to_missing_prot_info"].add(gene1 + gene2)
-        return {"data": None, "errors": errors}
-
-    try:
-        prot2 = wdi_core.WDItemEngine(wd_item_id=id_mapper[gene2]).get_wd_json_representation()["claims"][
-            WdProp.encodes
-        ]
-    except Exception as _e:
-        errors["missing_encodes_infos"].add(gene2)
-        errors["pairs_lost_due_to_missing_prot_info"].add(gene1 + gene2)
-        return {"data": None, "errors": errors}
-
-    if len(prot1) > 1:
-        errors["more_than_1_encodes"].add(gene1)
-        # return {"data": data, "errors": errors}
-    if len(prot2) > 1:
-        errors["more_than_1_encodes"].add(gene2)
-        # return {"data": data, "errors": errors}
-
-    try:
-        prot1_wdid = prot1[0]["mainsnak"]["datavalue"]["value"]["id"]
-        prot1_uniprot = prot1[0]["references"][0]["snaks"][WdProp.uniprot_id][0]["datavalue"]["value"]
-    except Exception as _e:
-        errors["missing_encodes_infos"].add(gene1)
-        return {"data": None, "errors": errors}
-
-    try:
-        prot2_wdid = prot2[0]["mainsnak"]["datavalue"]["value"]["id"]
-        prot2_uniprot = prot2[0]["references"][0]["snaks"][WdProp.uniprot_id][0]["datavalue"]["value"]
-    except Exception as _e:
-        errors["missing_encodes_infos"].add(gene2)
-        return {"data": None, "errors": errors}
-
-    oma_url1 = f"https://omabrowser.org/oma/vps/{prot1_uniprot}/"
-    oma_url2 = f"https://omabrowser.org/oma/vps/{prot2_uniprot}/"
-
-    if not is_oma_url_valid(oma_url1):
-        errors["oma_url_not_found"].add(oma_url1)
-        return {"data": None, "errors": errors}
-
-    if not is_oma_url_valid(oma_url2):
-        errors["oma_url_not_found"].add(oma_url2)
-        return {"data": None, "errors": errors}
-
-    return {
-        "data": {
-            "gene1": gene1,
-            "gene2": gene2,
-            "gene1_wdid": id_mapper[gene1],
-            "gene2_wdid": id_mapper[gene2],
-            "prot1_wdid": prot1_wdid,
-            "prot2_wdid": prot2_wdid,
-            "prot1_uniprot": prot1_uniprot,
-            "prot2_uniprot": prot2_uniprot,
-            "oma_url1": oma_url1,
-            "oma_url2": oma_url2,
-        },
-        "errors": {},
-    }
-
-
-if __name__ == "__main__":
+def main():
     start_time = time.time()
     parser = argparse.ArgumentParser(description="Process OMA ortholog pairs")
     parser.add_argument(
-        "nb_workers", type=int, nargs="?", default=None, help="Number of workers to use for parallel processing"
+        "--write", action="store_true", default=False, help="Enable write mode which requires login and password"
     )
     args = parser.parse_args()
+    write = args.write
 
-    if args.nb_workers:
-        pandarallel.initialize(nb_workers=args.nb_workers)
+    if "WDUSER" in os.environ and "WDPASS" in os.environ:
+        wd_user = os.environ["WDUSER"]
+        wd_login = wdi_login.WDLogin(user=wd_user, pwd=os.environ["WDPASS"])
     else:
-        pandarallel.initialize()
+        raise ValueError("WDUSER and WDPASS must be specified in `.env` file or as environment variables")
 
-    ensembl_mapper: dict = wdi_helpers.id_mapper(WdProp.ensembl_gene_id)
-    print(f"🔢 {len(ensembl_mapper)} Ensembl IDs found in Wikidata")
-    ncbigene_mapper: dict = wdi_helpers.id_mapper(WdProp.ncbi_gene_id)
-    print(f"🔢 {len(ncbigene_mapper)} NCBI gene IDs found in Wikidata")
-    id_mapper = {**ncbigene_mapper, **ensembl_mapper}
-    print(f"🔢 {len(id_mapper)} Ensembl and NCBI gene IDs found in Wikidata")
-    del ensembl_mapper
-    del ncbigene_mapper
+    wdi_core.WDItemEngine.setup_logging(
+        log_dir="./data/logs",
+        logger_name="WD_logger",
+        log_name=f"OmaOrthologBot-{datetime.now().strftime('%Y%m%d_%H:%M')}.log",
+    )
+    download_oma_files()
 
-    data_folder = "data"
+    # Map gene IDs to Wikidata IDs
+    # id_mapper = {**wdi_helpers.id_mapper(prop.entrez_gene_id), **wdi_helpers.id_mapper(prop.ensembl_gene_id)}
+    geneid_to_wdid: dict[str, str] = wdi_helpers.id_mapper(prop.ensembl_gene_id)
+    print(f"🔢 {len(geneid_to_wdid)} Ensembl IDs found in Wikidata")
+    # Map NCBI taxon ID to wikidata ID
+    taxon_to_wdid = wdi_helpers.id_mapper(prop.ncbi_taxonomy_id)
+
+    # NOTE: Mapping Wikidata gene IDs to Wikidata protein IDs. Does not work much because multiple proteins can be encoded by a gene
+    # gene_encodes_prot_mapper = {
+    #     v: k.replace("http://www.wikidata.org/entity/", "")
+    #     for k, v in wdi_helpers.id_mapper(prop.encodes).items()
+    # }
+
+    # first_five_items = {k: id_mapper[k] for k in list(id_mapper)[:5]}
+    # print(first_five_items)
+
     valid_orthos = []
-    combined_errors = defaultdict(set)
+    oma_url_found = set()
+    wd_items = {}
+    errors = defaultdict(set)
 
-    for filename in tqdm(os.listdir(data_folder), desc="Processing OMA orthologs files", unit="file"):
+    pattern = re.compile(r"orthologs_(\d+)-(\d+)\.csv")
+    for filename in tqdm(os.listdir(OMA_DIR), desc="Processing OMA orthologs files", unit="file"):
         if filename.endswith(".csv"):
             # NOTE: to filter for human orthologs add `and "9606" in filename`
             # print(f"📂 Processing {filename}")
-            file_path = os.path.join(data_folder, filename)
+            file_path = os.path.join(OMA_DIR, filename)
             try:
                 df = pd.read_csv(file_path)
             except Exception as _e:
-                combined_errors["error_loading_files"].add(filename)
+                errors["error_loading_files"].add(filename)
+                continue
+
+            match_taxon = pattern.search(filename)
+            if not match_taxon:
+                print(f"❌ Taxon not found in filename: {filename}")
+                continue
+
+            # Extract taxons from filename
+            taxon1, taxon2 = match_taxon.groups()
+            if taxon1 not in taxon_to_wdid:
+                errors["taxon_not_found"].add(taxon1)
+                continue
+            if taxon2 not in taxon_to_wdid:
+                errors["taxon_not_found"].add(taxon2)
                 continue
 
             # Find missing genes before filtering
-            missing_gene1 = set(df["gene1"].astype(str)) - set(id_mapper.keys())
-            missing_gene2 = set(df["gene2"].astype(str)) - set(id_mapper.keys())
-            combined_errors["gene_not_found"].update(missing_gene1)
-            combined_errors["gene_not_found"].update(missing_gene2)
+            missing_gene1 = set(df["gene1"].astype(str)) - set(geneid_to_wdid.keys())
+            missing_gene2 = set(df["gene2"].astype(str)) - set(geneid_to_wdid.keys())
+            errors["gene_not_found"].update(missing_gene1)
+            errors["gene_not_found"].update(missing_gene2)
 
             # Remove all rows for which the gene1 or gene2 are not in the id_mapper
-            df = df[(df["gene1"].astype(str).isin(id_mapper)) & (df["gene2"].astype(str).isin(id_mapper))]
+            df = df[(df["gene1"].astype(str).isin(geneid_to_wdid)) & (df["gene2"].astype(str).isin(geneid_to_wdid))]
             # If no rows remain after filtering, skip processing
             if df.empty:
                 continue
 
-            # NOTE: we could speed up the process by making a set out of the gene1 and gene2 columns
-            # and getting the wikidata IDs + prot IDs for these sets before running the row processing
+            for _i, row in df.iterrows():
+                gene1 = str(row["gene1"])
+                gene2 = str(row["gene2"])
 
-            results = df.parallel_apply(process_row, axis=1, args=(id_mapper,))
-            valid_orthos, combined_errors = aggregate_results(results, valid_orthos, combined_errors)
+                if gene1 not in wd_items:
+                    try:
+                        prot1 = wdi_core.WDItemEngine(wd_item_id=geneid_to_wdid[gene1]).get_wd_json_representation()[
+                            "claims"
+                        ][prop.encodes]
+                    except Exception as _e:
+                        errors["missing_encodes_infos"].add(gene1)
+                        errors["pairs_lost_due_to_missing_prot_info"].add(gene1 + gene2)
+                        continue
+                    wd_items[gene1] = prot1
+                else:
+                    prot1 = wd_items[gene1]
 
-    for error_type, errors in combined_errors.items():
-        print(f"⚠️ {error_type}: {len(errors)}")
+                if gene2 not in wd_items:
+                    try:
+                        prot2 = wdi_core.WDItemEngine(wd_item_id=geneid_to_wdid[gene2]).get_wd_json_representation()[
+                            "claims"
+                        ][prop.encodes]
+                    except Exception as _e:
+                        errors["missing_encodes_infos"].add(gene2)
+                        errors["pairs_lost_due_to_missing_prot_info"].add(gene1 + gene2)
+                        continue
+                    wd_items[gene2] = prot2
+                else:
+                    prot2 = wd_items[gene2]
+
+                # TODO: add for multiple proteins? Currently we just add for the 1st when many
+                if len(prot1) > 1:
+                    errors["more_than_1_encodes"].add(gene1)
+                if len(prot2) > 1:
+                    errors["more_than_1_encodes"].add(gene2)
+
+                try:
+                    prot1_wdid = prot1[0]["mainsnak"]["datavalue"]["value"]["id"]
+                    prot1_uniprot = prot1[0]["references"][0]["snaks"][prop.uniprot_id][0]["datavalue"]["value"]
+                except Exception as _e:
+                    errors["missing_encodes_infos"].add(gene1)
+                    continue
+
+                try:
+                    prot2_wdid = prot2[0]["mainsnak"]["datavalue"]["value"]["id"]
+                    prot2_uniprot = prot2[0]["references"][0]["snaks"][prop.uniprot_id][0]["datavalue"]["value"]
+                except Exception as _e:
+                    errors["missing_encodes_infos"].add(gene2)
+                    continue
+
+                oma_url1 = f"https://omabrowser.org/oma/vps/{prot1_uniprot}/"
+                oma_url2 = f"https://omabrowser.org/oma/vps/{prot2_uniprot}/"
+
+                if oma_url1 not in oma_url_found:
+                    if not is_oma_url_valid(oma_url1):
+                        errors["oma_url_not_found"].add(oma_url1)
+                        continue
+                    else:
+                        oma_url_found.add(oma_url1)
+
+                if oma_url2 not in oma_url_found:
+                    if not is_oma_url_valid(oma_url2):
+                        errors["oma_url_not_found"].add(oma_url2)
+                        continue
+                    else:
+                        oma_url_found.add(oma_url2)
+
+                # Generate references using OMA wikidata item + reference URL to OMA
+
+                # item1 = wdi_core.WDItemID(value=prot2_wdid, prop_nr=prop.ortholog, references=[(prop.stated_in, oma_wd_item), (prop.reference_url, oma_url1)])
+                # Docs: https://github.com/SuLab/WikidataIntegrator/blob/main/wikidataintegrator/wdi_core.py#L300
+                # https://github.com/SuLab/WikidataIntegrator/blob/main/wikidataintegrator/wdi_helpers/__init__.py#L59
+                item1 = wdi_core.WDItemEngine(
+                    wd_item_id=geneid_to_wdid[gene1],
+                    data=[
+                        wdi_core.WDItemID(
+                            geneid_to_wdid[gene2],
+                            prop.ortholog,
+                            references=[
+                                [
+                                    wdi_core.WDItemID(OMA_WD_ITEM_ID, prop.stated_in, is_reference=True),
+                                    wdi_core.WDExternalID(oma_url1, prop.reference_url, is_reference=True),
+                                ]
+                            ],
+                            qualifiers=[
+                                wdi_core.WDItemID(taxon_to_wdid[taxon2], prop.found_in_taxon, is_qualifier=True)
+                            ],
+                        )
+                    ],
+                    append_value=[prop.ortholog],
+                    global_ref_mode=GLOBAL_REF_MODE,
+                    # THIS OVERWRITES, we would need to write a custom ref handler to keep the existing references and only add the OMA browser reference when not present already
+                    # global_ref_mode="CUSTOM",
+                    # ref_handler=ref_handlers.update_retrieved_if_new,
+                    # TODO: the fast_run parameter could avoid writing items that already exist. But we want to update existing items with new references, so not really helpful here
+                    # fast_run_use_refs=True,
+                    # fast_run=True,
+                    # fast_run_base_filter={prop.ensembl_gene_id: "", prop.found_in_taxon: taxon_to_wdid[taxon1]},
+                    # core_props=core_props
+                )
+                wdi_helpers.try_write(
+                    item1,
+                    gene1,
+                    prop.ensembl_gene_id,
+                    edit_summary=EDIT_SUMMARY,
+                    login=wd_login,
+                    write=write,
+                )
+
+                # Add the reverse ortholog
+                item2 = wdi_core.WDItemEngine(
+                    wd_item_id=geneid_to_wdid[gene2],
+                    data=[
+                        wdi_core.WDItemID(
+                            geneid_to_wdid[gene1],
+                            prop.ortholog,
+                            references=[
+                                [
+                                    wdi_core.WDItemID(OMA_WD_ITEM_ID, prop.stated_in, is_reference=True),
+                                    wdi_core.WDExternalID(oma_url2, prop.reference_url, is_reference=True),
+                                ]
+                            ],
+                            qualifiers=[
+                                wdi_core.WDItemID(taxon_to_wdid[taxon1], prop.found_in_taxon, is_qualifier=True)
+                            ],
+                        )
+                    ],
+                    append_value=[prop.ortholog],
+                    global_ref_mode=GLOBAL_REF_MODE,
+                )
+                wdi_helpers.try_write(
+                    item2,
+                    gene2,
+                    prop.ensembl_gene_id,
+                    edit_summary=EDIT_SUMMARY,
+                    login=wd_login,
+                    write=write,
+                )
+
+                # print(json.dumps(item1.get_wd_json_representation(), indent=2))
+                # if "omabrowser" in json.dumps(item1.get_wd_json_representation(), indent=2):
+                #     print("omabrowser reference added in item1")
+                # if "omabrowser" in json.dumps(item2.get_wd_json_representation(), indent=2):
+                #     print("omabrowser reference added in item2")
+
+                valid_orthos.append(
+                    {
+                        "gene1": gene1,
+                        "gene2": gene2,
+                        "gene1_wdid": geneid_to_wdid[gene1],
+                        "gene2_wdid": geneid_to_wdid[gene2],
+                        "prot1_wdid": prot1_wdid,
+                        "prot2_wdid": prot2_wdid,
+                        "prot1_uniprot": prot1_uniprot,
+                        "prot2_uniprot": prot2_uniprot,
+                        "taxon1_wdid": taxon_to_wdid[taxon1],
+                        "taxon2_wdid": taxon_to_wdid[taxon2],
+                        "oma_url1": oma_url1,
+                        "oma_url2": oma_url2,
+                    }
+                )
+
+    for error_type, error_list in errors.items():
+        print(f"⚠️ {error_type}: {len(error_list)}")
     print(f"✅ {len(valid_orthos)} orthologs pairs found with proper OMA URL")
 
     valid_orthos_df = pd.DataFrame(valid_orthos)
     valid_orthos_df.to_csv("valid_ortholog_pairs.csv", index=False)
 
     print(f"⏱️ Total time taken: {(time.time() - start_time)/60:.2f} minutes")
+
+
+if __name__ == "__main__":
+    main()
